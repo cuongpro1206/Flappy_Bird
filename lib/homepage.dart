afa9ac0c8eb996419cd083e0b7ac3563a4aaab3b
cuongpro1206 <76671300+cuongpro1206@users.noreply.github.com>
2023-01-14 14:40:14 +0700

    Add files via upload

---
+++ lib/homepage.dart
@@ -0,0 +1,293 @@
+import 'dart:async';
+
+import 'package:flappy_bird/barrier.dart';
+import 'package:flappy_bird/bird.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  static double birdY = 0;
+  double initialPos = birdY;
+  double height = 0;
+  double time = 0;
+  double gravity = -4.9;
+  double velocity = 2.5;
+  double birdWidth = 0.3;
+  double birdHeight = 0.2;
+  double score = 0;
+  double best = 0;
+
+  //game setting
+  bool gameHasStarted = false;
+
+  // barrier variable
+  static List<double> barrierX = [2, 2 + 1.5];
+  static double barrierWidth = 0.3;
+  List<List<double>> barrierHeight = [
+    [0.6, 0.4],
+    [0.4, 0.6],
+  ];
+
+  void startGame() {
+    gameHasStarted = true;
+    Timer.periodic(Duration(milliseconds: 120), (timer) {
+      height = gravity * time * time + velocity * time;
+
+      setState(() {
+        birdY = initialPos - height;
+      });
+
+      // check bird dead
+      if (birdIsDead()) {
+        timer.cancel();
+        _showDialog();
+      }
+
+      //keep the map moving
+      moveMap();
+
+      // keep the time going
+      time += 0.1;
+    });
+  }
+
+  void moveMap() {
+    for (int i = 0; i < barrierX.length; i++) {
+      setState(() {
+        if (barrierX[i] < -1.5) {
+          barrierX[i] + 3;
+        }
+      });
+    }
+  }
+
+
+  void resetGame() {
+    Navigator.pop(context);
+    setState(() {
+      birdY = 0;
+      gameHasStarted = false;
+      time = 0;
+      initialPos = birdY;
+    });
+  }
+
+  void _showDialog() {
+    showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            backgroundColor: Colors.brown.shade500,
+            title: Center(
+              child: Column(
+                children: [
+                  Text(
+                    "G A M E  O V E R",
+                    style: TextStyle(color: Colors.white, fontSize: 35),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Container(
+                        height: 70,
+                        width: 100,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                        child: Column(
+                          children: [
+                            Text(
+                              "SCORE",
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 20),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        height: 70,
+                        width: 100,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                        child: Column(
+                          children: [
+                            Text(
+                              "BEST",
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 20),
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+            actions: [
+              GestureDetector(
+                onTap: resetGame,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(5),
+                  child: Container(
+                    padding: EdgeInsets.all(7),
+                    color: Colors.white,
+                    child: Text(
+                      "PLAY AGAIN",
+                      style: TextStyle(
+                        color: Colors.brown,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          );
+        });
+  }
+
+  void jump() {
+    setState(() {
+      time = 0;
+      initialPos = birdY;
+    });
+  }
+
+  bool birdIsDead() {
+    // check if the bird is hitting the top or the bottom
+    if (birdY < -1 || birdY > 1) {
+      return true;
+    }
+
+// check if the bird is hitting a barrier
+    for (int i = 0; i < barrierX.length; i++) {
+      if (barrierX[i] <= birdWidth &&
+          barrierX[i] + barrierWidth >= -birdWidth &&
+          (birdY <= -1 + barrierHeight[i][0] ||
+              birdY + birdHeight >= 1 - barrierHeight[i][1])) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: gameHasStarted ? jump : startGame,
+      child: Scaffold(
+        body: Column(
+          children: [
+            Expanded(
+              flex: 3,
+              child: Container(
+                color: Colors.blue,
+                child: Center(
+                  child: Stack(
+                    children: [
+                      MyBird(
+                        birdY: birdY,
+                        birdWidth: birdWidth,
+                        birdHeihgt: birdHeight,
+                      ),
+                      Mybarrier(
+                        barrierX: barrierX[0],
+                        barrierHeight: barrierHeight[0][0],
+                        barrierWidth: barrierWidth,
+                        isThisBottomBarrier: false,
+                      ),
+                      Mybarrier(
+                        barrierX: barrierX[0],
+                        barrierHeight: barrierHeight[0][1],
+                        barrierWidth: barrierWidth,
+                        isThisBottomBarrier: true,
+                      ),
+                      Mybarrier(
+                        barrierX: barrierX[1],
+                        barrierHeight: barrierHeight[1][0],
+                        barrierWidth: barrierWidth,
+                        isThisBottomBarrier: false,
+                      ),
+                      Mybarrier(
+                        barrierX: barrierX[1],
+                        barrierHeight: barrierHeight[1][1],
+                        barrierWidth: barrierWidth,
+                        isThisBottomBarrier: true,
+                      ),
+                      Container(
+                        alignment: Alignment(0, -0.5),
+                        child: Text(
+                          gameHasStarted ? '' : 'T A P  T O  P L A Y',
+                          style: TextStyle(
+                              fontSize: 30,
+                              fontWeight: FontWeight.w300,
+                              color: Colors.white),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              height: 25,
+              color: Colors.green.shade700,
+            ),
+            Expanded(
+                child: Container(
+              color: Colors.brown,
+              child: Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Text(
+                            "SCORE",
+                            style: TextStyle(fontSize: 25, color: Colors.white),
+                          ),
+                          Text(
+                            "0",
+                            style: TextStyle(fontSize: 25, color: Colors.white),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Text(
+                            "BEST",
+                            style: TextStyle(fontSize: 25, color: Colors.white),
+                          ),
+                          Text(
+                            "0",
+                            style: TextStyle(fontSize: 25, color: Colors.white),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            )),
+          ],
+        ),
+      ),
+    );
+  }
+}
